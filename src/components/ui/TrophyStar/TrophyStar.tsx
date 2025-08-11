3523690d14aa92672c9436ff335a2b606fe721ec
sensod9 <sensod9@mail.ru>
2025-08-11 15:19:07 +0200

    навёл красоту

--- src/components/ui/TrophyStar/TrophyStar.tsx
+++ src/components/ui/TrophyStar/TrophyStar.tsx
@@ -8,9 +8,14 @@ interface Props
 
 function TrophyStar({ phase = 0, title = ''}: Props) {
 	return (
-		<span className={`${styles.star} ${phase == 0 ? styles.phaseZero : phase == 1 ? styles.phaseOne : phase == 2 ? styles.phaseTwo : phase == 3 ? styles.phaseThree : phase == 4 ? styles.phaseFour : styles.phaseFive}`} title={title}>
-			{ phase ? <i className="fa-solid fa-star"></i> :
-			<i className="fa-regular fa-star" title={title}></i> }
+		<span className={`${styles.star}`}>
+			<span className={styles.title}>
+				{title}
+			</span>
+			<span className={`ms-2 ${phase == 0 ? styles.phaseZero : phase == 1 ? styles.phaseOne : phase == 2 ? styles.phaseTwo : phase == 3 ? styles.phaseThree : phase == 4 ? styles.phaseFour : styles.phaseFive}`}>
+				{ phase ? <i className="fa-solid fa-star"></i> :
+				<i className="fa-regular fa-star"></i> }
+			</span>
 		</span>
 	);
 }
